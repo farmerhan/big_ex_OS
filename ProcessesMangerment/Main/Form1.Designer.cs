c25b5fa7ea0227e6fd75c0df4e451fe4931aa430
shiba20212021 <shiba2021shiba2021@gmail.com>
2022-04-12 09:52:03 +0700

    GetAllProcessHasBeenDone

---
+++ ProcessesMangerment/Main/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ProcessesManangement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader columnHeader1;
+            this.listView_showProcs = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView_showProcs
+            // 
+            this.listView_showProcs.BackColor = System.Drawing.SystemColors.Menu;
+            this.listView_showProcs.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView_showProcs.HideSelection = false;
+            this.listView_showProcs.Location = new System.Drawing.Point(12, 12);
+            this.listView_showProcs.Name = "listView_showProcs";
+            this.listView_showProcs.Size = new System.Drawing.Size(788, 439);
+            this.listView_showProcs.TabIndex = 0;
+            this.listView_showProcs.UseCompatibleStateImageBehavior = false;
+            this.listView_showProcs.View = System.Windows.Forms.View.Details;
+            this.listView_showProcs.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "ProcessName";
+            columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PID";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Username";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Memory";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Description";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 463);
+            this.Controls.Add(this.listView_showProcs);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_showProcs;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ ProcessesMangerment/Main/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Required namespaces
+using System.Management;
+using System.Dynamic;
+using System.Diagnostics;
+
+namespace ProcessesManangement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // Khởi tạo đối tượng trong WDF
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Vũ
+            renderProcessesOnListView();
+            // Vũ
+
+        }
+
+        /*
+        ==========================================================================================
+                                                Phần xử lý của Vũ 
+        ==========================================================================================
+        */
+        /*
+         Do đối tượng Process không thể lấy đủ thông tin về các tiến trình,
+         vì vậy mà cần xử lý để lấy hai trường username(Người đang chạy tiến trình), và description (Thông tin chi tiết của tiến trình)
+         */
+        public ExpandoObject GetProcessExtraInformation(int processId)
+        {
+            // Mã truy vấn lấy mọi thông tin về tiến trình của lớp Win32_Process
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            // Tạo một đối tượng động để lưu trữ một vài thuộc tính mới lấy về
+            dynamic response = new ExpandoObject();
+            response.Description = "";
+            response.Username = "Unknown";
+
+            // Lặp lại đối tương ProcessList sau đó sẽ 
+            foreach (ManagementObject obj in processList)
+            {
+                // Lấy thông tin username
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                // Giá trị tại vị trí 0 là username
+                if (returnVal == 0)
+                {
+                    // Cho usename vào đối tương response
+                    response.Username = argList[0];
+                }
+
+                // Lấy thông tin chi tiết của tiến trình nếu tồn tại
+                if (obj["ExecutablePath"] != null)
+                {
+                    try
+                    {
+                        FileVersionInfo info = FileVersionInfo.GetVersionInfo(obj["ExecutablePath"].ToString());
+                        response.Description = info.FileDescription;
+                    }
+                    catch { }
+                }
+            }
+
+            // Trả về đối tượng bao gồm hai thuộc tính username và description
+            return response;
+        }
+
+        /*
+            Hàm chuyển đổi giá trị của số byte thực thành đơn vị dễ đọc 
+        */
+        public string BytesToReadableValue(long number)
+        {
+            List<string> suffixes = new List<string> { " B", " KB", " MB", " GB", " TB", " PB" };
+
+            for (int i = 0; i < suffixes.Count; i++)
+            {
+                long temp = number / (int)Math.Pow(1024, i + 1);
+
+                if (temp == 0)
+                {
+                    return (number / (int)Math.Pow(1024, i)) + suffixes[i];
+                }
+            }
+
+            return number.ToString();
+        }
+
+        // Hàm sẽ lấy mọi thông tin sau đó hiển thị tiến trình lên listview
+        public void renderProcessesOnListView()
+        {
+            // Tạo bảng lưu trữ tiến trình
+            Process[] processList = Process.GetProcesses();
+
+            // Tạo ảnh bạn sẽ đưa ra cho mọi tiến trình
+            ImageList Imagelist = new ImageList();
+
+            // Lặp qua mọi tiến trình để đưa ra những thông tin
+            foreach (Process process in processList)
+            {
+                // Định nghĩa trạng thái của tiến trình
+                string status = (process.Responding == true ? "Responding" : "Not responding");
+
+                // Lấy 2 thông tin về username và description mà lớp Process không có
+                dynamic extraProcessInfo = GetProcessExtraInformation(process.Id);
+
+                // Tạo mảng cái mà sẽ lưu thông tin được show ra
+                string[] row = {
+                    // 1 Tên tiến trình
+                    process.ProcessName,
+                    // 2 ID tiến trình
+                    process.Id.ToString(),
+                    // 3 Trạng thái tiến trình
+                    status,
+                    // 4 Tên người dùng người mà đang bật tiến trình
+                    extraProcessInfo.Username,
+                    // 5 Lượng bộ nhớ sử dụng
+                    BytesToReadableValue(process.PrivateMemorySize64),
+                    //process.PrivateMemorySize64.ToString(),
+                    // 6 Mô tả tiến trình
+                    extraProcessInfo.Description
+                };
+
+                //
+                // Nếu tiến trình không có ảnh, hành động này ngăn chặn ứng dụng bị lỗi
+                try
+                {
+                    // Thêm icon vào ImageList
+                    Imagelist.Images.Add(
+                        // Thêm một mã định danh cho mỗi icon của tiến trình (Nó giống như ID của tiến trình)
+                        process.Id.ToString(),
+                        // Thêm icon vào danh sách và chuyển đổi chúng thành Bitmap định dạng hiển thị trong window form
+                        // Dữ liệu hình ảnh của biểu tượng có thể được truy xuất bằng ***Icon.ExtractAssociatedIcon***
+                        Icon.ExtractAssociatedIcon(process.MainModule.FileName).ToBitmap()
+                    );
+                }
+                catch { }
+
+                // Tạo một Mục mới để thêm vào chế độ xem danh sách mà yêu cầu hàng thông tin là đối số đầu tiên
+                ListViewItem item = new ListViewItem(row) {
+                    // Đặt vị trí ảnh của mỗi item listView giống như được xác định trước trycatch
+                    ImageIndex = Imagelist.Images.IndexOfKey(process.Id.ToString())
+                };
+
+                // Thêm những item vào listView
+                listView_showProcs.Items.Add(item);
+            }
+
+            // Đặt imagelist trước mỗi một tiến trình
+            listView_showProcs.LargeImageList = Imagelist;
+            listView_showProcs.SmallImageList = Imagelist;
+        }
+
+        /*
+        ==========================================================================================
+                                                Phần xử lý của Vũ 
+        ==========================================================================================
+        */
+    }
+}

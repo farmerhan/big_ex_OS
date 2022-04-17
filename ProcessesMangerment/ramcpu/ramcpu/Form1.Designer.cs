a90e7d8072c3673e806df0c79436380aad5c819c
Linh <binhnguyen29122001@gmail.com>
2022-04-17 18:22:40 +0700

    Complete show DetailForm

---
+++ ProcessesMangerment/ramcpu/ramcpu/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace ramcpu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pRAM = new System.Diagnostics.PerformanceCounter();
+            this.pCPU = new System.Diagnostics.PerformanceCounter();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBarram = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBarcpu = new System.Windows.Forms.ProgressBar();
+            this.lbram = new System.Windows.Forms.Label();
+            this.lbcpu = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pRAM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pRAM
+            // 
+            this.pRAM.CategoryName = "Memory";
+            this.pRAM.CounterName = "% Committed Bytes in Use";
+            // 
+            // pCPU
+            // 
+            this.pCPU.CategoryName = "Processor";
+            this.pCPU.CounterName = "% Processor Time";
+            this.pCPU.InstanceName = "_Total";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RAM";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progressBarram
+            // 
+            this.progressBarram.Location = new System.Drawing.Point(92, 129);
+            this.progressBarram.Name = "progressBarram";
+            this.progressBarram.Size = new System.Drawing.Size(365, 27);
+            this.progressBarram.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "CPU";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // progressBarcpu
+            // 
+            this.progressBarcpu.Location = new System.Drawing.Point(92, 77);
+            this.progressBarcpu.Name = "progressBarcpu";
+            this.progressBarcpu.Size = new System.Drawing.Size(365, 27);
+            this.progressBarcpu.TabIndex = 2;
+            this.progressBarcpu.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // lbram
+            // 
+            this.lbram.AutoSize = true;
+            this.lbram.Location = new System.Drawing.Point(488, 143);
+            this.lbram.Name = "lbram";
+            this.lbram.Size = new System.Drawing.Size(21, 13);
+            this.lbram.TabIndex = 1;
+            this.lbram.Text = "0%";
+            this.lbram.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbcpu
+            // 
+            this.lbcpu.AutoSize = true;
+            this.lbcpu.Location = new System.Drawing.Point(488, 91);
+            this.lbcpu.Name = "lbcpu";
+            this.lbcpu.Size = new System.Drawing.Size(21, 13);
+            this.lbcpu.TabIndex = 1;
+            this.lbcpu.Text = "0%";
+            this.lbcpu.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(92, 208);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "CPU";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "RAM";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(417, 218);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Performance";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 510);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.progressBarcpu);
+            this.Controls.Add(this.lbcpu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBarram);
+            this.Controls.Add(this.lbram);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "RAM & CPU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pRAM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter pRAM;
+        private System.Diagnostics.PerformanceCounter pCPU;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBarram;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBarcpu;
+        private System.Windows.Forms.Label lbram;
+        private System.Windows.Forms.Label lbcpu;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
